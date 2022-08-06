8dcbec4c773efc336c000ea2ad739af37ea4af81
Danila Verbinskiy <danv4656@gmail.com>
2022-08-06 04:25:16 +0300

    Added 'home screen' folder and configured expo app

---
+++ src/screens/HomeScreen/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+import styles from './styles';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>HomeScreen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen;
---
+++ src/screens/HomeScreen/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native"
+
+const styles = StyleSheet.create({
+    container: {
+        alignContent: "center",
+        justifyContent: "center",
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: "bold",
+        textAlign: "center",
+        alignContent: "center",
+        justifyContent: "center",
+    },
+})
+
+export default styles;
