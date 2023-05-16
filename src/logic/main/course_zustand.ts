12c134aede473ed806e531e428dd433812288f6a
Danila Verbinskiy <work.danv4656@gmail.com>
2023-05-16 08:02:20 +0300

    feat: create course_zustand.ts, info_zustand.ts and tests_zustand.ts

---
+++ src/logic/main/course_zustand.ts
@@ -0,0 +1,5 @@
+import { create } from "zustand";
+
+const useCourse = create((set, get: any) => {
+    
+});
---
+++ src/logic/main/info_zustand.ts
@@ -0,0 +1,3 @@
+import { create } from "zustand";
+
+const useInfo = create((set, get: any) => {});
