b8a5982df361d9ae46a708ab039bb4b6813f3887
Danila Verbinskiy <work.danv4656@gmail.com>
2023-05-17 21:23:16 +0300

    update course_zustand.ts, create basic logic for work with course

--- src/logic/main/course_zustand.ts
+++ src/logic/main/course_zustand.ts
@@ -1,5 +1,45 @@
 import { create } from "zustand";
 
-const useCourse = create((set, get: any) => {
-    
-});
+const useCourse = create((set, get: any) => ({
+    id: "", // course id
+    title: "", // course title
+    description: "", // course description
+    levels: [], // course levels
+
+    // setters
+    setId: (id: string) => set({ id: id }),
+    setTitle: (title: string) => set({ title: title }),
+    setDescription: (description: string) => set({ description: description }),
+    setLevels: (levels: any) => set({ levels: levels }),
+
+    // getters
+    getId: () => get().id,
+    getTitle: () => get().title,
+    getDescription: () => get().description,
+    getLevels: () => get().levels,
+
+    // on load
+    onLoad: async () => {
+        const activeCourse = localStorage.getItem("activeCourse");
+        if (activeCourse) {
+            get().getCourse(activeCourse);
+        }
+    },
+
+    // change active course
+    changeActiveCourse: (id: string) => {
+        localStorage.setItem("activeCourse", id);
+        get().getCourse(id);
+    },
+
+    // reset
+    reset: () => set({ id: "", title: "", description: "", levels: [] }),
+
+    // api calls
+    getCourse: async (id: string) => {
+        // TODO: get course from api and set it to store
+    },
+    addCourseToUser: async (course_id: string, user_id: string) => {
+        // TODO: add course to user if user not have this course
+    },
+}));
