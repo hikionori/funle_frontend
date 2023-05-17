e33f8ee80a62c9e3290b6d89f12a397f2f3d3589
Danila Verbinskiy <work.danv4656@gmail.com>
2023-05-17 21:34:12 +0300

    update tests_zustand.ts

--- src/logic/main/tests_zustand.ts
+++ src/logic/main/tests_zustand.ts
@@ -1,3 +1,42 @@
 import { create } from "zustand";
 
-const useTests = create((set, get: any) => {});
+
+// TODO: think about how to store tests
+const useTests = create((set, get: any) => ({
+    // all tests data
+    tests: [], // tests
+
+    // corrent test data
+    id: "", // test id
+    question: "", // test title
+    answers: [], // test answer variants
+    answer: "", // test correct answer
+
+    // setters
+    setTests: (tests: any) => set({ tests: tests }),
+
+    // getters
+    getTests: () => get().tests,
+
+    // reset
+    reset: () => set({ ids: [], tests: [] }),
+
+    resetCorrentTest: () => set({ id: "", question: "", answers: [], answer: "" }),
+
+    // api calls
+    getTestsAPI: async (ids: string[]) => {
+        // TODO: get tests from api and set it to store
+        /*
+            For each id in ids:
+                get test from api
+                add test to tests
+        */
+    },
+
+
+
+    addTestToUser: async (test_id: string, user_id: string) => {
+        // TODO: add test to user if user not have this test
+    }
+
+}));
