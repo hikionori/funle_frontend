71fc6e1dd3ee8903bc806f1c6902633595b5d8e8
Danila Verbinskiy <work.danv4656@gmail.com>
2023-05-29 21:45:05 +0300

    create 1 file and update 1 file

--- src/view/screens/TestsScreen/index.tsx
+++ src/view/screens/TestsScreen/index.tsx
@@ -4,13 +4,15 @@ import {
 	TouchableOpacity,
 	Button,
 	ImageBackground,
-    Keyboard,
+	Keyboard,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import styles from "./style";
 import useTests from "../../../logic/main/tests_zustand";
+import Option from "../../components/TestsComponents/option";
+import { TextArea } from "native-base";
 
 // TODO: Add argument 'id'
 const TestsScreen = ({ navigation }: any) => {
@@ -25,10 +27,10 @@ const TestsScreen = ({ navigation }: any) => {
 		: null;
 	const progress = useTests((state: any) => state.progress);
 
-    // setters
-    const setProgress = useTests((state: any) => state.setProgress);
+	// setters
+	const setProgress = useTests((state: any) => state.setProgress);
 
-    const answer = useState(); // value of the answer
+	const [answer, setAnswer] = useState(""); // value of the answer
 
 	const [keyboardVisible, setKeyboardVisible] = useState(false);
 
@@ -36,7 +38,7 @@ const TestsScreen = ({ navigation }: any) => {
 	const handleAnswer = (text: string) => {
 		if (answer === activeTest.answer) {
 			progress[activeTestIndex] = 1;
-            // TODO: next test
+			// TODO: next test
 		} else {
 			progress[activeTestIndex] = 0;
 			// TODO: next test
@@ -54,9 +56,9 @@ const TestsScreen = ({ navigation }: any) => {
 	}, []);
 
 	//! debug
-    // useEffect(() => {
-    //     console.log("answer " + answer)
-    // }, [answer])
+	// useEffect(() => {
+	//     console.log("answer " + answer)
+	// }, [answer])
 
 	// TODO: Add more logic
 	useEffect(() => {
@@ -86,9 +88,8 @@ const TestsScreen = ({ navigation }: any) => {
 		});
 		//*
 
-        // fill progress array with two 0, 3 1 and 2 undefined
-        setProgress([0, 0, 1, 1, 1, undefined, undefined]);
-
+		// fill progress array with two 0, 3 1 and 2 undefined
+		setProgress([0, 0, 1, 1, 1, undefined, undefined]);
 	}, []);
 
 	return (
@@ -105,39 +106,38 @@ const TestsScreen = ({ navigation }: any) => {
 			>
 				<View style={styles.container}>
 					<View style={styles.boxGroup}>
-						{ progress.map((item: any, index: number) => {
-                            return (
-                                <View
-                                    key={index}
-                                    style={
-                                        // if item is 1, then show green box, else if 0 show red box, else show grey box
-                                        item === 1 ? (
-                                            styles.boxChecked
-                                        ) : item === 0 ? (
-                                            styles.boxUnchecked
-                                        ) : (
-                                            styles.box
-                                        )
-                                    }
-                                >
-                                    { // if item is 1, then show check icon, else if 0 show cross icon, else show nothing 
-                                        item === 1 ? (
-                                            <MaterialCommunityIcons
-                                                name="check"
-                                                size={15}
-                                                color="white"
-                                            />
-                                        ) : item === 0 ? (
-                                            <MaterialCommunityIcons
-                                                name="close"
-                                                size={15}
-                                                color="white"
-                                            />
-                                        ) : null   
-                                    }
-                                </View>
-                            );
-                        }) }
+						{progress.map((item: any, index: number) => {
+							return (
+								<View
+									key={index}
+									style={
+										// if item is 1, then show green box, else if 0 show red box, else show grey box
+										item === 1
+											? styles.boxChecked
+											: item === 0
+											? styles.boxUnchecked
+											: styles.box
+									}
+								>
+									{
+										// if item is 1, then show check icon, else if 0 show cross icon, else show nothing
+										item === 1 ? (
+											<MaterialCommunityIcons
+												name="check"
+												size={15}
+												color="white"
+											/>
+										) : item === 0 ? (
+											<MaterialCommunityIcons
+												name="close"
+												size={15}
+												color="white"
+											/>
+										) : null
+									}
+								</View>
+							);
+						})}
 					</View>
 					<View>
 						{/* //* Question text **/}
@@ -152,52 +152,36 @@ const TestsScreen = ({ navigation }: any) => {
 							Choose the right option:
 						</Text>
 						{/* //* Options **/}
-						<View style={styles.options}>
-							<TouchableOpacity style={styles.option}>
-								<MaterialCommunityIcons
-									name="circle-outline"
-									size={30}
-									style={{
-										marginLeft: 10,
-										marginRight: 10,
+						{activeTest.level === 1 ? (
+							<View style={styles.options}>
+								{activeTest.answers.map(
+									(item: any, index: number) => {
+										return (
+											<Option
+												key={index}
+												label={item}
+												value={item}
+												onPress={setAnswer}
+											/>
+										);
+									}
+								)}
+							</View>
+						) : activeTest.level === 2 ? (
+							<View style={styles.options}>
+								<TextArea
+									placeholder="Write your answer here"
+									autoCompleteType={"on"}
+									autoCorrect={false}
+									borderColor={"#E67B02"}
+									backgroundColor={"white"}
+									onChangeText={(text) => {
+										setAnswer(text);
 									}}
+									h={200}
 								/>
-								<Text style={styles.optionText}>2</Text>
-							</TouchableOpacity>
-							<TouchableOpacity style={styles.option}>
-								<MaterialCommunityIcons
-									name="circle-outline"
-									size={30}
-									style={{
-										marginLeft: 10,
-										marginRight: 10,
-									}}
-								/>
-								<Text style={styles.optionText}>4</Text>
-							</TouchableOpacity>
-							<TouchableOpacity style={styles.option}>
-								<MaterialCommunityIcons
-									name="circle-outline"
-									size={30}
-									style={{
-										marginLeft: 10,
-										marginRight: 10,
-									}}
-								/>
-								<Text style={styles.optionText}>6</Text>
-							</TouchableOpacity>
-							<TouchableOpacity style={styles.option}>
-								<MaterialCommunityIcons
-									name="circle-outline"
-									size={30}
-									style={{
-										marginLeft: 10,
-										marginRight: 10,
-									}}
-								/>
-								<Text style={styles.optionText}>5</Text>
-							</TouchableOpacity>
-						</View>
+							</View>
+						) : null}
 					</View>
 				</View>
 				<View style={styles.footer}>
