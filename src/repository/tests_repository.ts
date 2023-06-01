ad586dba9bb9276406a5a6710fda7482321eccde
Danila Verbinskiy <work.danv4656@gmail.com>
2023-06-01 21:29:51 +0300

    update tests_repository.ts

--- src/repository/tests_repository.ts
+++ src/repository/tests_repository.ts
@@ -2,53 +2,10 @@ import axios from "axios";
 import { baseUrl } from "./constants";
 
 const getTest = async (testId: string) => {
-    /*
-        response looks like this:
-        {
-            <TestType>: {
-                <test data>
-            }
-        }
-
-        example:
-        {
-            "ChoiceTest": {
-                "_id": {
-                    "$oid": "646876f2e3868812bd87133e"
-                },
-                "theme": "addition",
-                "question": "1 + 1",
-                "answers": [
-                    "2",
-                    "3",
-                    "4"
-                ],
-                "answer": "2",
-                "level": 1
-            }
-        }
-
-        should be returned this:
-            {
-                "_id": {
-                    "$oid": "646876f2e3868812bd87133e"
-                },
-                "theme": "addition",
-                "question": "1 + 1",
-                "answers": [
-                    "2",
-                    "3",
-                    "4"
-                ],
-                "answer": "2",
-                "level": 1
-            }
-
-    */
+    
     const response = await axios.get(
         `${baseUrl}/admin/get/test?id=${testId}`
     );
-    // TESTIT
     return response.data[Object.keys(response.data)[0]];
 };
 
